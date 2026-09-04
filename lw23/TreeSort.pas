bec7ebd561f0a32497f59b6c37d5a5d9dfd5700b
F1aner <meowmurovich@gmail.com>
2026-09-04 21:43:47 +0300

    Add files via upload

---
+++ lw23/TreeSort.pas
@@ -0,0 +1,55 @@
+PROGRAM TreeSort(INPUT, OUTPUT);
+
+TYPE
+  Tree = ^NodeType;
+  NodeType = RECORD
+               Ch: CHAR;
+               LLink, RLink: Tree 
+             END;
+             
+VAR
+  Root: Tree;
+  Ch: CHAR;
+
+PROCEDURE Insert(VAR Ptr: Tree; Data: CHAR);
+{Создаем новую ячейку в памяти и сравниваем значения для распределния по дереву}
+BEGIN {Insert}
+  IF (Ptr = NIL)
+  THEN
+    BEGIN
+      NEW(Ptr);
+      Ptr^.Ch := Data;
+      Ptr^.LLink := NIL;
+      Ptr^.RLink := NIL
+    END
+  ELSE
+    IF (Data < Ptr^.Ch)
+    THEN
+      Insert(Ptr^.LLink, Data)
+    ELSE
+      Insert(Ptr^.RLink, Data)
+END; {Insert}
+
+PROCEDURE PrintTree(Ptr: Tree);
+{Распечатывает дерево}
+BEGIN {PrintTree}
+  IF (Ptr <> NIL)
+  THEN 
+    BEGIN
+      PrintTree(Ptr^.LLink);
+      WRITE(Ptr^.Ch);
+      PrintTree(Ptr^.RLink)
+    END
+END; {PrintTree}
+
+BEGIN {TreeSort}
+  Root := NIL;
+  WHILE (NOT EOLN)
+  DO
+    BEGIN
+      READ(Ch);
+      Insert(Root, Ch)
+    END;
+  PrintTree(Root);
+  WRITELN
+END. {TreeSort}
